5e629b2fc027afc35890012a52782a605730dcef
jsonz1993 <jsonz@qq.com>
2022-04-20 17:27:28 +0800

    fix(basic): curly config

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -45,7 +45,7 @@ module.exports = {
 
     // common
     'semi': ['error', 'never'],
-    'curly': ['consistent'],
+    'curly': ['error', 'consistent'],
     'quotes': ['error', 'single'],
     'quote-props': ['error', 'consistent'],
     'no-console': ['warn'],
