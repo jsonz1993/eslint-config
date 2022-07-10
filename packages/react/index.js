dd98b9172e2c4d6eb75dedcdc4530cb70140fba3
jsonz1993 <jsonz@qq.com>
2022-07-10 16:36:30 +0800

    feat(react): add react-hooks rules

--- packages/react/index.js
+++ packages/react/index.js
@@ -3,12 +3,13 @@
  * @Date: 2022-01-18 16:17:25
  * @Email: jsonz@qq.com
  * @LastEditors: Jsonz
- * @LastEditTime: 2022-01-18 17:00:30
+ * @LastEditTime: 2022-07-10 16:33:09
  * @Description: file content
  */
 module.exports = {
   extends: [
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     '@jsonz/eslint-config-ts',
   ],
   settings: {
