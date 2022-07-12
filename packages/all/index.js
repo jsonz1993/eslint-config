90a0b46d363f7bd6c4f3a5b516455189771dc5d9
jsonz1993 <jsonz@qq.com>
2022-07-12 11:56:48 +0800

    fix: eslint extends order, prettier < basic < ts < react

--- packages/all/index.js
+++ packages/all/index.js
@@ -3,13 +3,13 @@
  * @Date: 2022-01-18 17:00:11
  * @Email: jsonz@qq.com
  * @LastEditors: Jsonz
- * @LastEditTime: 2022-07-06 14:17:08
+ * @LastEditTime: 2022-07-12 11:54:04
  * @Description: file content
  */
 
 module.exports = {
   extends: [
-    '@jsonz/eslint-config-react', 
     '@jsonz/eslint-config-prettier',
+    '@jsonz/eslint-config-react',
   ],
 }
